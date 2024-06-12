a2170a838cef29348b7cda0b48751467b0d5927a
imrulkayessifat <kayesimrul224@gmail.com>
2024-06-12 14:03:36 +0600

    compliance webhooks implements

--- src/controllers/compliance.controller.ts
+++ src/controllers/compliance.controller.ts
@@ -1,6 +1,9 @@
 const crypto = require('crypto')
 const getRawBody = require('raw-body')
+import { PrismaClient } from "@prisma/client";
 const webhooks_secret_key = process.env.WEBHOOKS_SECRET_KEY;
+
+const db = new PrismaClient();
 
 export const customerRequest = async (req: any, res: any) => {
     const shopDomain = req.get('x-shopify-shop-domain')
@@ -13,9 +16,21 @@ export const customerRequest = async (req: any, res: any) => {
         .update(body, 'utf8', 'hex')
         .digest('base64')
 
-    if (hmac === hash) { 
-        console.log("customer request");
-        JSON.parse(body.toString())
+    if (hmac === hash) {
+        try {
+            console.log(JSON.parse(body.toString()));
+            const data = db.store.findFirst({
+                where: {
+                    name: shopDomain
+                }
+            })
+            res.status(201).json({ data });
+        } catch (e) {
+            res.status(500).json({ error: 'An error occurred while storing product data.' });
+        }
+
+    } else {
+        res.status(403).json({ error: "you don't have access" })
     }
 }
 
@@ -30,10 +45,22 @@ export const customerErasure = async (req: any, res: any) => {
         .update(body, 'utf8', 'hex')
         .digest('base64')
 
-    if (hmac === hash) { 
-        console.log("customer erasure");
-        JSON.parse(body.toString())
-    }
+        if (hmac === hash) {
+            try {
+                console.log(JSON.parse(body.toString()))
+                const data = db.store.delete({
+                    where: {
+                        name: shopDomain
+                    }
+                })
+                res.status(201).json({ data });
+            } catch (e) {
+                res.status(500).json({ error: 'An error occurred while storing product data.' });
+            }
+    
+        } else {
+            res.status(403).json({ error: "you don't have access" })
+        }
 }
 
 export const shopErasure = async (req: any, res: any) => {
@@ -47,8 +74,20 @@ export const shopErasure = async (req: any, res: any) => {
         .update(body, 'utf8', 'hex')
         .digest('base64')
 
-    if (hmac === hash) { 
-        console.log("shop erasure");
-        console.log(JSON.parse(body.toString()))
-    }
+        if (hmac === hash) {
+            try {
+                console.log(JSON.parse(body.toString()))
+                const data = db.store.delete({
+                    where: {
+                        name: shopDomain
+                    }
+                })
+                res.status(201).json({ data });
+            } catch (e) {
+                res.status(500).json({ error: 'An error occurred while storing product data.' });
+            }
+    
+        } else {
+            res.status(403).json({ error: "you don't have access" })
+        }
 }
