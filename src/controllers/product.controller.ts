6173481f78bee56f3c1e3cff59631e4fab35d5b2
imrulkayessifat <kayesimrul224@gmail.com>
2024-08-07 17:44:37 +0600

    check webhooks1

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -27,7 +27,7 @@ const verifyRequest = async (req: Request) => {
 export const productCreate = async (req: any, res: any) => {
     try {
         console.log("webhook check ", "product create")
-        const body =  await getRawBody(req);;
+        const body = await getRawBody(req);;
         const { id, title } = body;
         const productId = id.toString();
         const shopDomain = req.get('x-shopify-shop-domain');
@@ -74,187 +74,189 @@ export const productUpdate = async (req: Request, res: Response): Promise<void> 
         console.log("hmac === hash", hmac === hash)
 
         // if (hmac === hash) {
-            try {
-                req.body = JSON.parse(body.toString());
-                const productData = req.body;
-                const { id, title, images, alt } = productData;
+        try {
+            req.body = JSON.parse(body.toString());
+            const productData = req.body;
+            const { id, title, images, alt } = productData;
 
-                const productId = id.toString();
+            const productId = id.toString();
 
-                const productInput = {
-                    id_storename: {
-                        id: productId,
-                        storename: shopDomain,
-                    },
-                };
+            const productInput = {
+                id_storename: {
+                    id: productId,
+                    storename: shopDomain,
+                },
+            };
 
-                await db.product.update({
-                    where: productInput,
-                    data: {
-                        product_vendor: productData.vendor,
-                        variant_title: productData.variants[0].title,
-                        product_page_title: productData.options[0].name,
-                        product_type: productData.product_type,
-                        product_barcode: productData.variants[0].barcode,
-                        product_title: productData.title,
-                        product_sku: productData.variants[0].sku,
-                    }
-                })
+            await db.product.update({
+                where: productInput,
+                data: {
+                    product_vendor: productData.vendor,
+                    variant_title: productData.variants[0].title,
+                    product_page_title: productData.options[0].name,
+                    product_type: productData.product_type,
+                    product_barcode: productData.variants[0].barcode,
+                    product_title: productData.title,
+                    product_sku: productData.variants[0].sku,
+                }
+            })
 
-                for (const image of images) {
-                    const { id: imageId, src: url, width, height, alt } = image;
-                    const imageIdStr = imageId.toString();
+            for (const image of images) {
+                const { id: imageId, src: url, width, height, alt } = image;
+                const imageIdStr = imageId.toString();
 
-                    console.log("image log", image)
+                console.log("image log", image)
+                console.log("alt", alt)
 
-                    const newUrl = new URL(url);
-                    const name = newUrl.pathname.split('/').pop() || null;
+                const newUrl = new URL(url);
+                const name = newUrl.pathname.split('/').pop() || null;
+                console.log("name", name)
 
-                    let responses = []
+                let responses = []
 
 
-                    if (alt !== null && alt.split('.')[0].split('-').pop().slice(-1) === 'C') {
-                        const response = await db.image.update({
-                            where: {
-                                uid: parseInt(alt.split('.')[0].split('-').pop().split('C').join(''))
-                            },
-                            data: {
-                                id: imageIdStr,
-                                url: url,
-                                name: alt || name,
-                                // alt: alt || name,
-                                // fileRename: false,
-                                // altRename: false,
-                                productId,
-                                status: 'COMPRESSED'
-                            }
-                        })
-                        responses.push(response);
-                    } else if (alt !== null && alt.split('.')[0].split('-').pop().slice(-1) === 'N') {
-                        const response = await db.image.update({
-                            where: {
-                                uid: parseInt(alt.split('.')[0].split('-').pop().split('N').join(''))
-                            },
-                            data: {
-                                id: imageIdStr,
-                                url: url,
-                                name: alt || name,
-                                // alt: alt || name,
-                                // fileRename: false,
-                                // altRename: false,
-                                productId,
-                                status: 'NOT_COMPRESSED'
-                            }
-                        })
-                        responses.push(response);
-                    } else {
-
-                        const imageExit = await db.image.findFirst({
-                            where: {
-                                id: imageIdStr
-                            }
-                        })
-
-                        if (!imageExit) {
-                            const response = await db.image.create({
-                                data: {
-                                    id: imageIdStr,
-                                    url,
-                                    name: alt || name,
-                                    alt: alt || name,
-                                    fileRename: false,
-                                    altRename: false,
-                                    productId,
-                                    status: 'NOT_COMPRESSED',
-                                    storename: shopDomain
-                                },
-                            });
-                            responses.push(response);
-                        }
-
-                    }
-
-                    io.emit('image_model', () => {
-                        console.log('an event occured in shopify product image upload');
-                    });
-
-                    const storeRes = await db.store.findFirst({
+                if (alt !== null && alt.split('.')[0].split('-').pop().slice(-1) === 'C') {
+                    const response = await db.image.update({
                         where: {
-                            name: shopDomain
+                            uid: parseInt(alt.split('.')[0].split('-').pop().split('C').join(''))
+                        },
+                        data: {
+                            id: imageIdStr,
+                            url: url,
+                            name: alt || name,
+                            // alt: alt || name,
+                            // fileRename: false,
+                            // altRename: false,
+                            productId,
+                            status: 'COMPRESSED'
+                        }
+                    })
+                    responses.push(response);
+                } else if (alt !== null && alt.split('.')[0].split('-').pop().slice(-1) === 'N') {
+                    const response = await db.image.update({
+                        where: {
+                            uid: parseInt(alt.split('.')[0].split('-').pop().split('N').join(''))
+                        },
+                        data: {
+                            id: imageIdStr,
+                            url: url,
+                            name: alt || name,
+                            // alt: alt || name,
+                            // fileRename: false,
+                            // altRename: false,
+                            productId,
+                            status: 'NOT_COMPRESSED'
+                        }
+                    })
+                    responses.push(response);
+                } else {
+
+                    const imageExit = await db.image.findFirst({
+                        where: {
+                            id: imageIdStr
                         }
                     })
 
-                    if (storeRes.autoCompression) {
-                        const imageRes = await db.image.findFirst({
-                            where: {
-                                id: imageIdStr
-                            }
-                        })
-                        if (imageRes.status === 'NOT_COMPRESSED') {
-                            const response = fetch(`${process.env.MQSERVER}/image/compress-image`, {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                },
-                                body: JSON.stringify({
-                                    uid: imageRes.uid,
-                                    productid: imageRes.productId,
-                                    url: imageRes.url,
-                                    storeName: shopDomain
-                                })
-                            });
-                        }
-                    }
-                    if (storeRes.autoFileRename) {
-                        const imageRes = await db.image.findFirst({
-                            where: {
-                                id: imageIdStr
-                            }
-                        })
-
-                        if (imageRes.id) {
-                            const req = fetch(`${process.env.MQSERVER}/rename/file-rename`, {
-                                method: 'PUT',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                },
-                                body: JSON.stringify({
-                                    storeName: shopDomain,
-                                    id: `${imageRes.id}`,
-                                })
-                            })
-                        }
+                    if (!imageExit) {
+                        const response = await db.image.create({
+                            data: {
+                                id: imageIdStr,
+                                url,
+                                name: alt || name,
+                                alt: alt || name,
+                                fileRename: false,
+                                altRename: false,
+                                productId,
+                                status: 'NOT_COMPRESSED',
+                                storename: shopDomain
+                            },
+                        });
+                        responses.push(response);
                     }
 
-                    if (storeRes.autoAltRename) {
-                        const imageRes = await db.image.findFirst({
-                            where: {
-                                id: imageIdStr
-                            }
-                        })
+                }
 
-                        if (imageRes.id) {
-                            const req = fetch(`${process.env.MQSERVER}/rename/alt-rename`, {
-                                method: 'PUT',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                },
-                                body: JSON.stringify({
-                                    storeName: shopDomain,
-                                    id: `${imageRes.id}`,
-                                })
-                            })
+                io.emit('image_model', () => {
+                    console.log('an event occured in shopify product image upload');
+                });
+
+                const storeRes = await db.store.findFirst({
+                    where: {
+                        name: shopDomain
+                    }
+                })
+
+                if (storeRes.autoCompression) {
+                    const imageRes = await db.image.findFirst({
+                        where: {
+                            id: imageIdStr
                         }
+                    })
+                    if (imageRes.status === 'NOT_COMPRESSED') {
+                        const response = fetch(`${process.env.MQSERVER}/image/compress-image`, {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                uid: imageRes.uid,
+                                productid: imageRes.productId,
+                                url: imageRes.url,
+                                storeName: shopDomain
+                            })
+                        });
+                    }
+                }
+                if (storeRes.autoFileRename) {
+                    const imageRes = await db.image.findFirst({
+                        where: {
+                            id: imageIdStr
+                        }
+                    })
+
+                    if (imageRes.id) {
+                        const req = fetch(`${process.env.MQSERVER}/rename/file-rename`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                storeName: shopDomain,
+                                id: `${imageRes.id}`,
+                            })
+                        })
                     }
                 }
 
+                if (storeRes.autoAltRename) {
+                    const imageRes = await db.image.findFirst({
+                        where: {
+                            id: imageIdStr
+                        }
+                    })
 
-                res.status(200).json({ data: [] });
-
-            } catch (e) {
-                console.error(e);
-                res.status(500).json({ error: 'An error occurred while updating product data.' });
+                    if (imageRes.id) {
+                        const req = fetch(`${process.env.MQSERVER}/rename/alt-rename`, {
+                            method: 'PUT',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                storeName: shopDomain,
+                                id: `${imageRes.id}`,
+                            })
+                        })
+                    }
+                }
             }
+
+
+            res.status(200).json({ data: [] });
+
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({ error: 'An error occurred while updating product data.' });
+        }
         // } else {
         //     res.status(403).json({ error: "you don't have access" })
         // }
